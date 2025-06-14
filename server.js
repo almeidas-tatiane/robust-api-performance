e5734fb240ba83373969a5427cbaeb11e6250e0c
almeidas-tatiane <almeidas.tatiane@gmail.com>
2025-06-13 21:35:57 -0300

    Changing port from 3000 to 3001

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '1h';
 const mongoUri = process.env.MONGO_URI;
